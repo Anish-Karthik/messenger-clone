cfd6db0bc72470475ecc5e0f95628686eed0d17c
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-01-13 16:11:49 +0530

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
 import TRPCProvider from "./_trpc/Provider"
-
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
--- prettier.config.js
+++ prettier.config.js
@@ -22,8 +22,11 @@ module.exports = {
     "",
     "^[./]",
   ],
-  importOrderSeparation: true,
+  importOrderSeparation: false,
   importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
 }
