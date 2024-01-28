888602e054b278f136cd9443e23164881ec8c0d9
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-01-28 17:32:22 +0530

    added UI carousal for images ready to be uploaded

--- components/chat/message-form.tsx
+++ components/chat/message-form.tsx
@@ -1,14 +1,29 @@
 "use client"
 
 import React, { useState } from "react"
+import Image from "next/image"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { ImageIcon, SendHorizonalIcon } from "lucide-react"
+import {
+  ImageIcon,
+  PlusCircleIcon,
+  SendHorizonalIcon,
+  Trash2Icon,
+  XCircleIcon,
+} from "lucide-react"
 import { useForm } from "react-hook-form"
 import toast from "react-hot-toast"
 import * as z from "zod"
 
 import { cn } from "@/lib/utils"
 import { useUploadThing } from "@/lib/utils/uploadthing"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 import {
   Form,
   FormControl,
@@ -18,6 +33,8 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+
+import { Button } from "../ui/button"
 
 interface MessageFormProps extends React.HTMLAttributes<HTMLButtonElement> {
   // add your custom props here
@@ -53,40 +70,104 @@ const MessageForm: React.FC<MessageFormProps> = ({ ...props }) => {
     console.log(values)
   }
 
-  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleImageChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    appendTo: boolean
+  ) {
     e.preventDefault()
     const fileReader = new FileReader()
 
     if (e.target?.files && e.target.files.length > 0) {
       const file = e.target?.files[0]
-      setFiles(Array.from(e.target.files))
-      if (!file.type.includes("image")) return
-
-      fileReader.onload = async (event) => {
-        const imageDataUrl = event.target?.result?.toString() || ""
-        console.log(imageDataUrl)
+      if (appendTo) {
+        setFiles((prev) => [...prev, ...Array.from(e.target.files || [])])
+      } else {
+        setFiles(Array.from(e.target.files || []))
       }
-
+      if (!file.type.includes("image")) return
       fileReader.readAsDataURL(file)
     }
   }
+
   console.log(files)
   console.log(form.getValues("files"))
   console.log(isValid)
   return (
-    <div className={cn("border-t p-4 shadow-sm", props.className)}>
+    <div className={cn("relative border-t p-4 shadow-sm", props.className)}>
+      {files.length > 0 && (
+        <div className="absolute bottom-[4.4rem] left-8 z-50 h-fit w-64 rounded-md bg-slate-200 px-12 sm:max-w-sm">
+          <Carousel className="w-full max-w-xs ">
+            <CarouselContent>
+              {files.map((file, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <div className="mb-1 flex justify-end">
+                      <Button
+                        variant={"ghost"}
+                        onClick={() => {
+                          setFiles((prev) => prev.filter((_, i) => i !== index))
+                          setFiles((prev) => [...prev])
+                        }}
+                      >
+                        <Trash2Icon className="-mx-4" />
+                      </Button>
+                    </div>
+                    <Card className="mb-2 !p-0">
+                      <CardContent className="flex aspect-square items-end justify-center !p-0">
+                        {
+                          <Image
+                            key={file.name}
+                            src={URL.createObjectURL(file)}
+                            alt={file.name}
+                            width={200}
+                            height={200}
+                            className=""
+                          />
+                        }
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      )}
+
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex items-center gap-4"
         >
+          {files.length > 0 && (
+            <Button
+              type="button"
+              variant="ghost"
+              onClick={() => setFiles([])}
+              className="-mr-3 text-red-400"
+            >
+              <XCircleIcon className="-mx-2" />
+            </Button>
+          )}
           <FormField
             control={form.control}
             name="files"
             render={({ field }) => (
               <FormItem>
                 <FormLabel htmlFor="file-input">
-                  <ImageIcon className="text-blue-400" height={30} width={30} />
+                  {files.length > 0 ? (
+                    <div className="rounded-full bg-sky-500 p-1 py-[0.3rem] text-white hover:opacity-70">
+                      <PlusCircleIcon />
+                    </div>
+                  ) : (
+                    <ImageIcon
+                      className="text-blue-400"
+                      height={30}
+                      width={30}
+                    />
+                  )}
                 </FormLabel>
                 <FormControl>
                   <Input
@@ -96,21 +177,19 @@ const MessageForm: React.FC<MessageFormProps> = ({ ...props }) => {
                     multiple={true}
                     className="hidden"
                     name={field.name}
-                    onChange={(e) => handleImageChange(e)}
+                    onChange={(e) => handleImageChange(e, files.length > 0)}
                   />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="message"
             render={({ field }) => (
               <FormItem className="w-full">
-                {/* <FormLabel htmlFor="file-input">
-                  <ImageIcon className="text-blue-400" height={30} width={30} />
-                </FormLabel> */}
                 <FormControl>
                   <Input
                     type="text"
--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 
 import { db } from "@/lib/db"
+import { pusherServer } from "@/lib/pusher"
 
 import { publicProcedure, router } from "./trpc"
 
@@ -186,17 +187,130 @@ const conversationsRouter = router({
     .mutation(async ({ input: { id, name } }) => {
       await db.conversation.update({ where: { id }, data: { name } })
     }),
-  messages: publicProcedure.input(z.string()).query(async ({ input: id }) => {
-    return await db.conversation
-      .findUnique({ where: { id } })
-      .messages({ include: { sender: true, seen: true } })
-  }),
+  getAllMessages: publicProcedure
+    .input(z.string())
+    .query(async ({ input: id }) => {
+      return await db.conversation
+        .findUnique({ where: { id } })
+        .messages({ include: { sender: true, seen: true } })
+    }),
+  setSeen: publicProcedure
+    .input(
+      z.object({
+        conversationId: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ input: { conversationId, userId } }) => {
+      const conversation = await db.conversation.findUnique({
+        where: {
+          id: conversationId,
+        },
+        include: {
+          messages: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            take: 1,
+            include: {
+              sender: true,
+              seen: true,
+            },
+          },
+          users: true,
+        },
+      })
+
+      if (!conversation) {
+        throw new Error("Conversation not found")
+      }
+
+      // Find last message
+      const lastMessage = conversation.messages[0]
+
+      if (!lastMessage) {
+        return conversation
+      }
+
+      // Update seen of last message
+      const updatedMessage = await db.message.update({
+        where: {
+          id: lastMessage.id,
+        },
+        include: {
+          sender: true,
+          seen: true,
+        },
+        data: {
+          seen: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      })
+      return conversation
+    }),
+})
+export const messagesRouter = router({
+  create: publicProcedure
+    .input(
+      z.object({
+        conversationId: z.string(),
+        senderId: z.string(),
+        body: z.string(),
+        image: z.string().url(),
+      })
+    )
+    .mutation(async ({ input: { conversationId, senderId, body, image } }) => {
+      const newMessage = await db.message.create({
+        data: {
+          conversationId,
+          senderId,
+          body,
+          image,
+          seen: { connect: { id: senderId } },
+        },
+        include: {
+          sender: true,
+          seen: true,
+        },
+      })
+      // update the conversation with the new message
+      const updatedConversation = await db.conversation.update({
+        where: { id: conversationId },
+        data: {
+          lastMessageAt: newMessage.createdAt,
+          messages: {
+            connect: { id: newMessage.id },
+          },
+        },
+        include: {
+          users: true,
+        },
+      })
+      await pusherServer.trigger(conversationId, "messages:new", newMessage)
+
+      // last message is the new message without the sender
+      let lastMessage = { ...newMessage, sender: undefined }
+
+      updatedConversation.users.map((user) => {
+        if (user.id === senderId) return
+        pusherServer.trigger(user.email!, "conversation:update", {
+          id: conversationId,
+          messages: [lastMessage],
+        })
+      })
+
+      return newMessage
+    }),
 })
 
 export const appRouter = router({
   test: publicProcedure.query(() => {
     return { message: "Hello World" }
   }),
+  messages: messagesRouter,
   users: usersRouter,
   conversations: conversationsRouter,
 })
