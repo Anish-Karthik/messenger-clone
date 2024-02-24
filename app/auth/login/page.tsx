439f3edfd505f4219739c3fcae9fcd5812baa95b
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-02-24 18:51:21 +0530

    Too redirect fixing attempt 1

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,12 @@
 import { LoginForm } from "@/components/auth/login-form"
+import { currentUser } from "@/lib/auth"
+import { redirect } from "next/navigation"
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const user = await currentUser()
+  if (user) {
+    redirect("/conversations")
+  }
   return <LoginForm />
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import { redirect } from "next/navigation"
 
-import { currentUser } from "@/lib/auth"
 
 export default async function Home() {
-  const user = await currentUser()
-  if (!user) {
-    redirect("/auth/login")
-  } else {
-    redirect("/conversations")
-  }
+  redirect("/auth/login")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   )
