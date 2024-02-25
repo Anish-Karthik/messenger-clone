2aa593d032212c2a5d9a427a1ebb2903065dd97b
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-02-25 23:21:54 +0530

    Auth too many redirects fix

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,10 @@
-import { redirect } from "next/navigation"
-import { ClipLoader } from "react-spinners"
 
-import { currentUser } from "@/lib/auth"
+import { LoginForm } from "@/components/auth/login-form"
 
 export default async function Home() {
-  const user = await currentUser()
-  if (!user) {
-    redirect("/auth/login")
-  } else {
-    redirect("/conversations")
-  }
   return (
     <div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <ClipLoader />
+      <LoginForm />
     </div>
   )
 }
